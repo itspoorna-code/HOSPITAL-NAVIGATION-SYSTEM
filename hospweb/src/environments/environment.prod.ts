22b4becf11fc5bae9799d8f1cf5966c73476a7c8
itspoorna-code <itspoorna011@gmail.com>
2025-02-24 22:41:16 +0530

    Removed hospweb from tracking

--- hospweb/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-// src/environments/environment.prod.ts
-
-export const environment = {
-    production: true,  // Set to true for production
-    apiUrl: 'https://your-production-api-url.com'  // Your Flask backend URL for production
-  };
-  
